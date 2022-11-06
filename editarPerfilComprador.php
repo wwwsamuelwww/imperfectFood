695e1b7e03aff9a89dd08aa4d8b8cdc0ddbc0b70
LimbertQ <102129028+LimbertQ@users.noreply.github.com>
2022-11-06 12:04:55 -0400

    Cambios en la estructura html

--- editarPerfilComprador.php
+++ editarPerfilComprador.php
@@ -26,3 +26,17 @@ array_pop($resultado);
 
 
 <!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+    integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous" />
+  <link rel="stylesheet" href="css/estiloFormularioCompradores.css">
+  <title>Imperfect Food</title>
+</head>
+<body>
