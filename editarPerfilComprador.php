b78d7073e7a1463494fcc94348b9bb1b5ca7ebdb
RaisaVO <115442239+RaisaVO@users.noreply.github.com>
2022-11-06 00:42:16 -0400

    Se añadio la estructura HTML

---
+++ editarPerfilComprador.php
@@ -0,0 +1,14 @@
+<?php
+
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+session_start();
+$ema = $_SESSION['email1'];
+$pas = $_SESSION['pass1'];
