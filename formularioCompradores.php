c818275e6bd5e10ef185b36f31731b3303f6e29f
CesarCanaza <115328729+CesarCanaza@users.noreply.github.com>
2022-11-16 23:33:57 -0400

    Formulario Compradores
    Arreglo en validacion "Ingrese un nombre valido"

--- formularioCompradores.php
+++ formularioCompradores.php
@@ -46,8 +46,8 @@
                   <div class="col">
                     <label for="NombreNegocio" class="form-label">Nombre:</label>
                     <input type="text" name="NombreNegocio" id="NombreNegocio" class="form-control"
-                      pattern="^[A-Z|a-z|0-9|`|&|.|\s|!|-|,]{3,20}$" required>
-                    <div class="invalid-feedback">
+                      pattern="^[A-Z|a-z|0-9|`|&|.|\s|!|-|,]{3,40}$" required>
+                    <div id = "validacion1" class="invalid-feedback">
                       Ingrese un nombre valido
                     </div>
       
