7e651d79c80ab4545a5c97bd772339a2549a22c5
LimbertQ <102129028+LimbertQ@users.noreply.github.com>
2022-12-20 12:58:13 -0400

    Se corrigió la consulta de la DB

--- conexiones/sacarDatosParaVentana.php
+++ conexiones/sacarDatosParaVentana.php
@@ -7,3 +7,10 @@
        session_start();
        $idusuario = $_SESSION['idUsuario'];
        $resultado2 = mysqli_query($conn,"SELECT * FROM carrito, productos WHERE carrito.idproducto = productos.id AND carrito.idcomprador = '$idusuario'");
+       $resultadoFinal = "";
+       foreach($resultado2 as $row2){
+                    
+                $resultadoFinal = $resultadoFinal.'<p> '.$row2['cantidad'].' '.$row2['NombreDeProducto'].' '.$row2['subtotal']. ' bs</p>';
+               
+        }
+        echo "$resultadoFinal";
