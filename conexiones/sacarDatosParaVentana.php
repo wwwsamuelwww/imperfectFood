c6753152a28446aa9e6f3e472b0a561eb287c640
LimbertQ <102129028+LimbertQ@users.noreply.github.com>
2022-12-20 13:06:25 -0400

    Se modifico el archivo para sacar datos

--- conexiones/sacarDatosParaVentana.php
+++ conexiones/sacarDatosParaVentana.php
@@ -8,9 +8,10 @@
        $idusuario = $_SESSION['idUsuario'];
        $resultado2 = mysqli_query($conn,"SELECT * FROM carrito, productos WHERE carrito.idproducto = productos.id AND carrito.idcomprador = '$idusuario'");
        $resultadoFinal = "";
-       foreach($resultado2 as $row2){
+        foreach($resultado2 as $row2){
                     
-                $resultadoFinal = $resultadoFinal.'<p> '.$row2['cantidad'].' ' bs</p>';
+                $resultadoFinal = $resultadoFinal.'<p> '.$row2['cantidad'].' '.$row2['NombreDeProducto'].' '.$row2['subtotal']. ' bs</p>';
                
         }
         echo "$resultadoFinal";
+?>
