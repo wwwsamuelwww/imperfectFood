6805b54cd71548e8d4c248af60de5cce54db7065
wwwsamuelwww <115440451+wwwsamuelwww@users.noreply.github.com>
2022-12-20 22:47:23 -0400

    creando agregar carrito php

---
+++ agregarCarrito.php
@@ -0,0 +1,9 @@
+<?php
+    $servername = "localhost";
+    $database = "imperfectfood";     
+    $username = "root";
+    $password = "";
+    $conn = mysqli_connect($servername, $username, $password, $database);
+    $idcapturado = $_POST['valorNumero'];
+    session_start();
+    $idusuario = $_SESSION['idUsuario'];
