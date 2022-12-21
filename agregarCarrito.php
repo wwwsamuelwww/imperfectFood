11dfb2ceb295623ab6291ff4c86acf4521bbbe92
wwwsamuelwww <115440451+wwwsamuelwww@users.noreply.github.com>
2022-12-20 22:47:53 -0400

    terminando agregar carrito php

--- agregarCarrito.php
+++ agregarCarrito.php
@@ -7,3 +7,12 @@
     $idcapturado = $_POST['valorNumero'];
     session_start();
     $idusuario = $_SESSION['idUsuario'];
+$resultadoSiExisteProducto = mysqli_query($conn,"SELECT * FROM carrito WHERE carrito.idcomprador = '$idusuario' AND carrito.idproducto = '$idcapturado'");
+    if (mysqli_num_rows($resultadoSiExisteProducto) === 0){
+        $precioOfertaProducto = mysqli_query($conn,"SELECT productos.PrecioDeOferta FROM productos WHERE productos.id = '$idcapturado'");
+        $row = mysqli_fetch_row($precioOfertaProducto);
+        $precio = $row[0];
+        $precioConvertido = floatval($precio);
+        $resultado = mysqli_query($conn,"INSERT INTO carrito(idcomprador,idproducto,cantidad,subtotal) VALUES ('$idusuario','$idcapturado',1,'$precioConvertido')");    }
+?>
+                  
