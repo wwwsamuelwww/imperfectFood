b8b8436d5d6281f8bf05798ce6c32c32fd8d0f76
wwwsamuelwww <115440451+wwwsamuelwww@users.noreply.github.com>
2022-12-20 22:50:15 -0400

    terminandi eliminar carrito php

--- eliminarCarrito.php
+++ eliminarCarrito.php
@@ -7,3 +7,7 @@
     $idcapturado = $_POST['idProducto'];
     session_start();
     $idusuario = $_SESSION['idUsuario'];
+ $resultado = mysqli_query($conn,"DELETE FROM carrito WHERE carrito.idcomprador ='$idusuario' AND carrito.idproducto = '$idcapturado'");
+    $url= '../carrito.php';
+    echo '<META HTTP-EQUIV=REFRESH CONTENT="1; '.$url.'">';
+?>
