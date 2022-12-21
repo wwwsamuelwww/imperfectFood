232e82dcdca9cc59066fc09844e1da273e968376
wwwsamuelwww <115440451+wwwsamuelwww@users.noreply.github.com>
2022-12-20 22:49:10 -0400

    terminando borrar carrito php

--- borrarCarrito.php
+++ borrarCarrito.php
@@ -6,3 +6,7 @@
     $conn = mysqli_connect($servername, $username, $password, $database);
     session_start();
     $idusuario = $_SESSION['idUsuario'];
+$resultado = mysqli_query($conn,"DELETE FROM carrito WHERE carrito.idcomprador = '$idusuario'");
+    $url= '../paginaPrincipalCompradores.php';
+    echo '<META HTTP-EQUIV=REFRESH CONTENT="1; '.$url.'">';
+?>
