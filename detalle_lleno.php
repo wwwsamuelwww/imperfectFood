e0a088c3b1b737fbd53540420ec9e9c2c738f682
wwwsamuelwww <115440451+wwwsamuelwww@users.noreply.github.com>
2023-04-02 16:27:39 -0400

    correccion del correcto

---
+++ detalle_lleno.php
@@ -0,0 +1,23 @@
+<div class="vender-detalle-lleno">
+    <div class="titulo-detalle">
+        <span>Detalle de Venta</span>
+    </div>
+
+    <div class="contenedor-items">
+        <!-- con js se agregera acodigo html aqui -->
+    </div>
+
+    <div class="detalle-gif">
+        <lottie-player src="https://assets1.lottiefiles.com/packages/lf20_wd1udlcz.json"  background="transparent"  speed="1"  style="width: 300px; height: 300px;"  loop  autoplay></lottie-player>
+        <p>Presiona sobre cualquier producto para añadirlo a tu detalle de venta</p>
+    </div>
+
+    <div class="detalle-total ocultar">
+        <div class="fila">
+            <strong>Total: </strong>
+            <strong>Bs. <span class="detalle-precio-total">0</span></strong>
+        </div>
+        <button class="boton-registrar-venta">Registrar Venta</button>
+        <button onclick="vaciar()" class="boton-cancelar-registro-venta">Cancelar</button>
+    </div>
+</div>
